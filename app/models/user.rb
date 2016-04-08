2b3689ba36fc1bae583134755c49e003dccec4fd
Nokitoo <guigui4700@gmail.com>
2016-04-08 13:18:34 +0200

    add Notifier Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
 	:recoverable, :rememberable, :trackable, :validatable
+	after_create :send_registration_notification
 
 	has_many :notifications
 	has_one :wallet, dependent: :destroy
@@ -11,6 +12,10 @@ class User < ActiveRecord::Base
 
 	def name
 		firstname + ' ' + lastname
+	end
+
+	def send_registration_notification
+		Notifier.send_registration(self).deliver
 	end
 
 end
