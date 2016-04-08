962eeeae1119eba675b1a24eed13b335081684a3
cchardeau <corentin.chardeau@epitech.eu>
2016-04-08 17:12:03 +0200

    Update finalization of an order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330125047) do
+ActiveRecord::Schema.define(version: 20160408125350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160330125047) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "match",       default: false
+    t.integer  "delivery_id"
   end
 
   create_table "notifications", force: :cascade do |t|
