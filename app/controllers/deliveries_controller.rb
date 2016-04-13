6c211b9a5e2291f471de5d22fcaaee28f8ab9616
Nokitoo <guigui4700@gmail.com>
2016-04-13 13:29:36 +0200

    change /deliveries/:delivery_id/finalize return status

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -133,7 +133,7 @@ class DeliveriesController < BaseController
 
         Rating.create!(to_user_id: @delivery.availability.deliveryman_id, from_user_id: @delivery.delivery_request.buyer_id, rating: params[:rating].to_i)
         format.html { render :new }
-        format.json { render json: { notice: 'RATING_DONE' }, status: :unprocessable_entity }
+        format.json { render json: { notice: 'RATING_DONE' }, status: :ok }
 
       # Mauvais code de validation
       else
