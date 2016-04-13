d75a92603a4018bd00a3cea3faa554d69e9e71b9
Nokitoo <guigui4700@gmail.com>
2016-04-13 18:00:56 +0200

    fix Delivery notif

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,14 +1,11 @@
 class Delivery < ActiveRecord::Base
-
-	belongs_to :availability
-	belongs_to :delivery_request
-
 	has_many :delivery_contents, foreign_key: 'id_delivery'
 	has_one :availability, foreign_key: 'id', primary_key: 'availability_id'
 	has_one :delivery_request, foreign_key: 'id', primary_key: 'delivery_request_id'
 
 	after_create :generate_validation_code
 	after_create :send_accepted_delivery
+	before_create :check_duplicate
 
 	after_save :calculate_commission
 	after_save :calculate_shipping_total
@@ -24,6 +21,12 @@ class Delivery < ActiveRecord::Base
 		charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
 		self.validation_code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
 		self.save
+	end
+
+	def check_duplicate
+		if Delivery.where(delivery_request_id: self.delivery_request.id).count > 0
+			raise "This delivery has already been accepted"
+		end
 	end
 
 	def send_accepted_delivery
@@ -46,9 +49,12 @@ class Delivery < ActiveRecord::Base
 			meta[:shop] = response
 		end
 
-		@others = Delivery.joins(:availability).where('availabilities.schedule_id = ? AND availabilities.shop_id = ? AND availabilities.deliveryman_id != ?', @availability.schedule_id, @availability.shop_id, @availability.deliveryman_id)
+		@others = Availability.where('schedule_id = ? AND shop_id = ? AND deliveryman_id != ?', @availability.schedule_id, @availability.shop_id, @availability.deliveryman_id)
+		# @others = Delivery.joins(:availability).where(availabilities: {schedule_id: @availability.schedule_id}, availabilities: {shop_id: @availability.shop_id}).where.not(availabilities: {delivery_id: @availability.deliveryman_id})
+		ap "OTHERS"
+		ap @others
 		@others.each do |other|
-			Notification.find_by(delivery_id: other.id).update(mode: 'outdated_delivery', title: 'Cette livraison n\'est plus disponible', content: 'Cette livraison n\'est plus disponible')
+			Notification.find_by(user_id: other.deliveryman_id).update(mode: 'outdated_delivery', title: 'Cette livraison n\'est plus disponible', content: 'Cette livraison n\'est plus disponible')
 		end
 
 		Notification.create! mode: 'accepted_delivery', title: 'La demande a été acceptée par un livreur', content: 'La demande a été acceptée par un livreur', sender: 'sms', user_id: @delivery_request.buyer_id, meta: meta.to_json, read: false, delivery_id: self.id
