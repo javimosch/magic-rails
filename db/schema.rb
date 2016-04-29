c33949da15a633a4b2403b797c3e36c1e509b490
Nokitoo <guigui4700@gmail.com>
2016-04-29 09:42:20 +0200

    don't cancel delivery if already done + migration

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,22 @@ ActiveRecord::Schema.define(version: 20160428122613) do
     t.datetime "updated_at",          null: false
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
   create_table "deliveries", force: :cascade do |t|
     t.string   "status",              default: "pending"
     t.string   "validation_code"
