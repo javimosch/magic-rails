96e7b1dc75e9638eb4be1602a951df438225d446
cchardeau <corentin.chardeau@epitech.eu>
2016-05-31 11:32:27 +0200

    Add logo and address in mail signature

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,11 +1,5 @@
 class Notifier < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notifier.send_notification.subject
-  #
-
   def send_registration(user)
     @user = user
 
